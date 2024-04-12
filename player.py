4568fc59f51a5925e760fd1822d3962a2be8f1d3
kgerot <kgerot@fastmail.com>
2024-04-12 18:03:32 -0500

    data and track structure built with metadata recognition

---
+++ player.py
@@ -0,0 +1,14 @@
+import tkinter, sv_ttk
+import pandas as pd
+import data_processing as dat
+
+
+
+files = dat.find_music()
+
+# for file, subfolder in files:
+#     print(f"File: {file}, Subfolder: {subfolder}")
+
+# root = tkinter.Tk()
+# sv_ttk.set_theme("dark")
+# root.mainloop()
